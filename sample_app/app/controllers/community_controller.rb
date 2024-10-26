8755518dcc4d06e4912433d610568e6e3e1ba0dd
bp20009 <83199984+bp20009@users.noreply.github.com>
2024-10-26 08:27:14 +0000

    Develop community controller and message controller

---
+++ sample_app/app/controllers/community_controller.rb
@@ -0,0 +1,33 @@
+class CommunityController < ApplicationController
+    def create
+        community = Community.new(community_params)
+        if community.save
+          render json: community, status: :created
+        else
+          render json: community.errors, status: :unprocessable_entity
+        end
+      end
+    
+      def show
+        community = Community.find(params[:id])
+        render json: {
+            id: community.id,
+            keywords: community.keywords,
+            users: community.users,
+            messages: community.messages
+        }
+      end
+    
+      def join
+        community = Community.find(params[:id])
+        user = User.find(params[:user_id])
+        community.users << user unless community.users.include?(user)
+        render json: community
+      end
+    
+      private
+    
+      def community_params
+        params.require(:community).permit(:name, keywords: [])
+      end
+end
