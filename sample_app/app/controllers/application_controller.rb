66282b0fc5e7e13bb0ea499510e2943b98baf7e7
bp20009 <83199984+bp20009@users.noreply.github.com>
2024-10-26 05:29:35 +0000

    Update user controller

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "hello, world!"
-  end
+ #rubyとruby on railsの書き方を間違えた
+  # def initialize
+ #   @users = {}
+ #  @communities = {}
+ #  @messages = {}
+ # end
+ # def regester_user(name,email)
+ #   user = User.new(name,email)
+ #   @users[user.user_id]=user
+ #   user
+ # end
+ # def get_user_profile(user_id)
+ #   user = @users[user_id]
+ #   return "User not found" unless user
+ #
+ #   UserProfile.new(user)
+ # end
 end
