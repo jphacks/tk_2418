a3b1f81aaf8bbdfdb7ece02d821ada7db6386608
YukiUmemoto <120009206+YukiUmemoto@users.noreply.github.com>
2024-10-26 17:07:47 +0000

    UPDATE: routes.rb

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,25 +1,23 @@
 Rails.application.routes.draw do
-  namespace :api do
-    # Users routes
-    get '/users/index', to: 'users#index'
-    post '/users', to: 'users#create'
-    get '/users/:id', to: 'users#show'
-    put '/users/:id', to: 'users#update'
-    delete '/users/:id', to: 'users#destroy'
+  # Users routes
+  get '/api/users/index', to: 'users#index'
+  post '/api/users', to: 'users#create'
+  get '/api/users/:id', to: 'users#show'
+  put '/api/users/:id', to: 'users#update'
+  delete '/api/users/:id', to: 'users#destroy'
 
-    # Communities routes
-    post '/communities', to: 'communities#create'                  # Create a new community
-    get '/communities/:id', to: 'communities#show'                 # Get a community by ID
-    post '/communities/:id/join', to: 'communities#join'           # Join a community
+  # Communities routes
+  post '/api/communities', to: 'communities#create'                  # Create a new community
+  get '/api/communities/:id', to: 'communities#show'                 # Get a community by ID
+  post '/api/communities/:id/join', to: 'communities#join'           # Join a community
 
-    # Messages routes
-    get '/communities/:community_id/messages', to: 'messages#index'   # Get all messages in a community
-    post '/communities/:community_id/messages', to: 'messages#create' # Create a new message in a community
+  # Messages routes
+  get '/api/communities/:community_id/messages', to: 'messages#index'   # Get all messages in a community
+  post '/api/communities/:community_id/messages', to: 'messages#create' # Create a new message in a community
 
-    # Papers routes
-    get '/papers', to: 'papers#index'
-    post '/papers', to: 'papers#create'
-    get '/papers/:id', to: 'papers#show'
-    get '/papers/recommend', to: 'papers#recommend'
-  end
+  # Papers routes
+  get '/api/papers', to: 'papers#index'
+  post '/api/papers', to: 'papers#create'
+  get '/api/papers/:id', to: 'papers#show'
+  get '/api/papers/recommend', to: 'papers#recommend'
 end
